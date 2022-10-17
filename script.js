6740d057abd977d88efbee4d8e2fc0f32b4b9c65
Vaidahazan <116027754+Vaidahazan@users.noreply.github.com>
2022-10-17 21:15:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function afiseazaText(){
+    document.querySelector("#experienta1").classList.toggle("element-ascuns");
+    //document.querySelector=ne returneaza dcumentul,classlist =ne ofera o lista cu clasele
+    //toggle=va adauga o clasa in cazul in care nu este in lista ,si o va sterge daca este in lista (se aplica doar pentru CLASSLIST)
+    
+}
+function afiseazaText2(){
+    document.querySelector("#experienta2").classList.toggle("element-ascuns");
+}
